ca9bb5e6618a12c540c89947261868c8e97a1471
ankitraj <ankit.raj@ticket.se>
2024-10-26 13:21:21 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,39 @@
+/**
+ * 
+ *      <div id="parent">
+ *          <div id="child">
+ *              <h1>I'm h1 tag</h1> 
+ *              <h2>I'm h2 tag</h1> 
+ *          </div>
+ *          <div id="child2">
+ *              <h1>I'm h1 tag</h1> 
+ *              <h2>I'm h2 tag</h1> 
+ *          </div>
+ *      </div>
+ * 
+ */
+
+
+// const heading = React.createElement("h1", 
+//     {id : "heading"}, 
+//     "Hello World from React!");
+
+//     console.log(heading);
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(heading);
+
+const parent = React.createElement("div", { id: "parent" },
+
+   [ React.createElement("div", { id: "child" },
+    [React.createElement("h1", {}, "I'm an h1 tag"), React.createElement("h2", {}, "I'm an h2 tag")]
+    ),
+    React.createElement("div", { id: "child2" },
+    [React.createElement("h1", {}, "I'm an h1 tag"), React.createElement("h2", {}, "I'm an h2 tag")]
+    )]
+
+);
+
+console.log(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
